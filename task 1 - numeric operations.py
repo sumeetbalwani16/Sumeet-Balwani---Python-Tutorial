a0cc5a6564589c5f20008fec688d71c2737b470e
sumeetbalwani16 <sumeet.balwani@escalent.co>
2026-01-16 20:17:21 +0530

    Add files via upload

---
+++ task 1 - numeric operations.py
@@ -0,0 +1,11 @@
+a = int(input("Enter your first number = "))
+b = int(input("Enter your second number = "))
+Addition = a + b
+print("Addition", Addition)
+Subtraction = a - b
+print("Subtraction", Subtraction)
+Multiplication = a * b
+print("Multiplication", Multiplication)
+Division = a / b
+print("Division", round(Division, 2))
+
